116be85a782d64bf5bd0f1b85e2136544c7e8f58
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2001-11-23 09:21:53 +0000

    *** empty log message ***

---
+++ src/framework/RubyCocoa.h
@@ -0,0 +1,16 @@
+/** -*-objc-*-
+ *
+ *   $Id$
+ *
+ *   Copyright (c) 2001 FUJIMOTO Hisakuni <hisa@imasy.or.jp>
+ *
+ *   This program is free software.
+ *   You can distribute/modify this program under the terms of
+ *   the GNU Lesser General Public License version 2.
+ *
+ **/
+
+#import <RubyCocoa/RBRuntime.h>
+#import <RubyCocoa/RBProxy.h>
+#import <RubyCocoa/ocdata_conv.h>
+
