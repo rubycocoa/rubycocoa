753b323a3fa5f8f5fee5cf7548c56cbe2b791e3f
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2001-11-25 09:27:44 +0000

    *** empty log message ***

---
+++ src/sample/mk_app1/MyView.rb
@@ -0,0 +1,25 @@
+require 'osx/ocobject'
+
+class MyView < OSX::OCObject
+
+  ib_loadable :NSView
+  derived_methods 'drawRect:'
+
+  def initialize
+    @color_name = 'blue'
+  end
+
+  def drawRect(frame)
+    color_set = "OSX::NSColor.#{@color_name}Color.set"
+    eval (color_set)
+    OSX.NSRectFill(bounds)
+  end
+
+  def set_color(color_name)
+    @color_name = color_name.to_s.downcase
+    lockFocus
+    drawRect(bounds)
+    unlockFocus
+  end
+
+end
