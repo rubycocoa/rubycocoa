1426d4db4fbfbb6eb82dd15bdb23e782a5870c7b
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2001-11-26 01:28:38 +0000

    *** empty log message ***

--- src/post-config.rb
+++ src/post-config.rb
@@ -1,11 +1,17 @@
-print <<MSG_DEFINE
+def libruby_installed?
+  /ld.*Undefined\s+symbols/ =~ `cc -framework LibRuby`
+end
+
+unless libruby_installed? then
+  print <<MSG_DEFINE
 !!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!! begin Note !!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!
 if "LibRuby.framework" is not installed, require install it before
 execute setup. type:
 
-  % ruby tool/mk_libruby_frmwk.rb  # install to /Library/Frameworks
+  % ruby tool/install-libruby-frmwk.rb  # install to /Library/Frameworks
       or
-  % ruby tool/mk_libruby_frmwk.rb {anywhere}  # install to anywhere
+  % ruby tool/install-libruby-frmwk.rb {anywhere}  # install to anywhere
 MSG_DEFINE
+end
