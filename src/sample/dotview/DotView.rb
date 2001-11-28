529fb383e498f347167048314d0f9be416a45e34
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2001-11-28 00:14:34 +0000

    *** empty log message ***

---
+++ src/sample/dotview/DotView.rb
@@ -0,0 +1,48 @@
+require 'osx/ocobject'
+
+class DotView < OSX::OCObject
+
+  ib_loadable :NSView
+  ib_outlets :colorWell, :sizeSlider
+  derived_methods 'drawRect:', 'isOpaque', 'mouseUp:'
+
+  def initialize
+    @center = OSX::NSPoint.new (50, 50)
+    @color = OSX::NSColor.redColor
+    @radius = 10.0
+  end
+
+  def awakeFromNib
+    @colorWell.setColor (@color)
+    @sizeSlider.setFloatValue (@radius)
+  end
+
+  def drawRect (rect)
+    OSX::NSColor.whiteColor.set
+    OSX::NSRectFill (bounds)
+    dot_rect = OSX::NSRect.new (@center.x - @radius, @center.y - @radius,
+			       2 * @radius, 2 * @radius)
+    @color.set
+    OSX::NSBezierPath.bezierPathWithOvalInRect(dot_rect).fill
+  end
+
+  def isOpaque
+    true
+  end
+
+  def mouseUp (event)
+    @center = convertPoint (event.locationInWindow, :fromView, nil)
+    setNeedsDisplay true
+  end
+
+  def setColor (sender)
+    @color = sender.color
+    setNeedsDisplay true
+  end
+
+  def setRadius (sender)
+    @radius = sender.floatValue
+    setNeedsDisplay true
+  end
+
+end
