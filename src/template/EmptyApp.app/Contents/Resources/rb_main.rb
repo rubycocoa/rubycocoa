64bd259806c133442728cb60177453ff8ea3c9d6
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2001-11-28 02:38:42 +0000

    add template directory

---
+++ src/template/EmptyApp.app/Contents/Resources/rb_main.rb
@@ -0,0 +1,32 @@
+#
+# $Id$
+#
+
+require 'osx/cocoa'
+
+######################
+#### BEGIN CONFIG ####
+######################
+BUNDLE_NAME = :MainMenu
+
+RUBY_SOURCES = [
+
+]
+######################
+##### END CONFIG #####
+######################
+
+def rb_main_init
+  RUBY_SOURCES.each {|src| require src.to_s }
+end
+
+def ns_app_main
+  app = OSX::NSApplication.sharedApplication
+  OSX::NSBundle.loadNibNamed_owner (BUNDLE_NAME.to_s, app)
+  OSX.NSApp.run
+end
+
+if $0 == __FILE__ then
+  rb_main_init
+  ns_app_main
+end
