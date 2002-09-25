002f557fe4ba0e2577ca5b5a94d4c87b6aff3d4d
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2002-09-25 05:04:17 +0000

    define class OCMessageSendException. add argument to OCException#initialize.

--- src/lib/osx/objc/oc_exception.rb
+++ src/lib/osx/objc/oc_exception.rb
@@ -16,17 +16,21 @@ module OSX
 
     attr_reader :name, :reason, :userInfo, :nsexception
 
-    def initialize(ocexcp)
+    def initialize(ocexcp, msg = nil)
       @nsexception = ocexcp
       @name = @nsexception.ocm_send(:name).to_s
       @reason = @nsexception.ocm_send(:reason).to_s
       @user_info = @nsexception.ocm_send(:userInfo)
-      super "#{@name}"
+      msg = "#{@name} - #{@reason}" if msg.nil?
+      super (msg)
     end
 
   end
 
   class OCDataConvException < RuntimeError
+  end
+
+  class OCMessageSendException < RuntimeError
   end
 
 end
