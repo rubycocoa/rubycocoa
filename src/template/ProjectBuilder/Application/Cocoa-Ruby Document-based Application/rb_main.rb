cd0c38ffc4be51128a3f67194cfc6c82e46b9fed
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2002-12-17 05:02:29 +0000

    move templates for ProjectBuilder to directory named ProjectBuilder

---
+++ src/template/ProjectBuilder/Application/Cocoa-Ruby Document-based Application/rb_main.rb
@@ -0,0 +1,20 @@
+#
+#  rb_main.rb
+#  ÇPROJECTNAMEÈ
+#
+#  Created by ÇFULLUSERNAMEÈ on ÇDATEÈ.
+#  Copyright (c) ÇYEARÈ ÇORGANIZATIONNAMEÈ. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+def rb_main_init
+  path = OSX::NSBundle.mainBundle.resourcePath.to_s
+  rbfiles = Dir.glob(File.join(path, '*.rb')) - [ __FILE__ ]
+  rbfiles.each {|path| require(path) }
+end
+
+if $0 == __FILE__ then
+  rb_main_init
+  OSX.NSApplicationMain(0, nil)
+end
