8f90aa1c0554452821ce085e112b1104b9fc60ec
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2002-12-22 08:15:10 +0000

    In Ruby 1.8 (after 2002.9.27), 'NSObject#ns_overrides' may be called more first than 'Class#inherited'.

--- src/framework/src/ruby/osx/objc/oc_import.rb
+++ src/framework/src/ruby/osx/objc/oc_import.rb
@@ -18,11 +18,11 @@ module OSX
     if not OSX.const_defined?(sym) then
       const_name = sym.to_s
       sym_name = ":#{sym}"
-      OSX.module_eval %[
+      OSX.module_eval %{
         clsobj = NSClassFromString(#{sym_name})
         rbcls = class_new_for_occlass(clsobj)
         #{const_name} = rbcls if rbcls
-      ]
+      }
     end
   end
   module_function :ns_import
@@ -30,37 +30,44 @@ module OSX
   # create Ruby's class for Cocoa class
   def OSX.class_new_for_occlass(occls)
     klass = Class.new(OSX::ObjcID)
-    klass.instance_eval %[
+    klass.class_eval %{
       include OCObjWrapper
+      self.extend OCObjWrapper
+      self.extend NSBehaviorAttachment
       @ocid = #{occls.__ocid__}
-    ]
-    klass.extend OCObjWrapper
-
-    def klass.new() alloc.init end
+    }
     def klass.__ocid__() @ocid end
     def klass.to_s() name end
-
-    def klass.inherited(kls)
-      spr_name = self.name.split('::')[-1]
-      kls_name = kls.name.split('::')[-1]
-      occls = OSX.objc_derived_class_new(kls, kls_name, spr_name)
-      kls.instance_eval "@ocid = #{occls.__ocid__}"
-      kls.extend NSBehaviorAttachment
-
-      # restrict creating an instance by Class#new.
-      def kls.new
-	raise "use 'alloc.initXXX' to instantiate Cocoa Object"
-      end
-    end
-
-    klass
+    def klass.inherited(subklass) subklass.ns_inherited() end
+    return klass
   end
 
   module NSBehaviorAttachment
 
+    # restrict creating an instance by Class#new of NSObject gruop.
+    def new
+      raise "use 'alloc.initXXX' to instantiate Cocoa Object"
+    end
+
+    # initializer for definition of a derived class of a class on
+    # Objective-C World.
+    def ns_inherited()
+      return if @inherited
+      spr_name = superclass.name.split('::')[-1]
+      kls_name = self.name.split('::')[-1]
+      occls = OSX.objc_derived_class_new(self, kls_name, spr_name)
+      self.instance_eval "@ocid = #{occls.__ocid__}"
+      @inherited = true
+    end
+
     # declare to override instance methods of super class which is
     # defined by Objective-C.
     def ns_overrides(*args)
+      # In Ruby 1.8 (after 2002.9.27), this method may be called more
+      # first than 'Class#inherited'.
+      ns_inherited()
+
+      # insert specified selectors to Objective-C method table.
       args.each do |name|
 	name = name.to_s.gsub('_',':')
 	OSX.objc_derived_class_method_add(self, name)
