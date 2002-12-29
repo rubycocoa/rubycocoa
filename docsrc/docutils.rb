56ec55777f42b927a98f3f99d0aef05368c22176
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2002-12-29 10:41:00 +0000

    news management feature

--- docsrc/docutils.rb
+++ docsrc/docutils.rb
@@ -83,23 +83,20 @@ class PageTemplate
     @doc.to_s
   end
 
-end
+end				# class PageTemplate
 
 
 
 class RDDocument
 
-  attr_reader :src_name, :title, :body
+  attr_reader :src_name, :title, :body, :lang, :charset
 
-  def initialize(fname)
+  def initialize( fname, lang, charset )
     @src_name = fname
-    charset = "us-ascii"
-    lang = "en"
-    if /\.ja\z/ =~ fname then
-      charset = "euc-jp"
-      lang = "ja"
-    end
-    rd2(fname, "/tmp/rubycocoahoge" , lang)
+    @lang = lang
+    @charset = charset
+
+    rd2(fname, "/tmp/rubycocoahoge")
     xml_src = `cat /tmp/rubycocoahoge`
     command 'rm -f /tmp/rubycocoahoge'
     @doc = REXML::Document.new(xml_src)
@@ -121,19 +118,77 @@ class RDDocument
     raise(RuntimeError, cmd) unless system(cmd)
   end
 
-  def rd2(src, dst, lang, css = nil, charset = nil)
-    unless charset then
-      charset = case lang
-		when 'en' then "us-ascii"
-		when 'ja' then "euc-jp"
-		end
-    end
-    css = "rdtext.css" unless css
-    opt_lang = "--html-lang=#{lang}" if lang
-    opt_charset = "--html-charset=#{charset}" if charset
-    opt_css = "--with-css=#{css}" if css
-    cmdstr = "rd2 -r rd/rd2html-lib #{opt_css} #{opt_charset} #{opt_lang}"
+  def rd2(src, dst)
+    opt_lang = "--html-lang=#{@lang}" if @lang
+    opt_charset = "--html-charset=#{@charset}" if @charset
+    cmdstr = "rd2 -r rd/rd2html-lib #{opt_charset} #{opt_lang}"
     command "#{cmdstr} #{src} > #{dst}"
   end
 
-end
+end				# class RDDocument
+
+
+
+class NewsArticle
+
+  attr_reader :subject_ja, :subject_en, :date, :uid,
+    :body_ja, :body_en
+
+  def parse( input )
+    begin
+      return nil unless parse_header( input )
+      parse_body( input, 'ja' ) if @subject_ja
+      parse_body( input, 'en' ) if @subject_en
+      return self
+    rescue EOFError
+      return nil
+    end
+  end
+
+  def parse_header( input )
+    line = ""
+    line = input.readline while /^$/ =~ line
+    until /^$/ =~ line do
+      case line
+      when /^subject-ja:(.*)$/i then @subject_ja = $1.strip
+      when /^subject-en:(.*)$/i then @subject_en = $1.strip
+      when /^date:(.*)$/i then @date = $1.strip
+      when /^uid:(.*)$/i then @uid = $1.strip
+      else
+	raise "#{self.class}#parse_header: #{line.chop}"
+      end
+      line = input.readline
+    end
+    return true
+  end
+
+  def parse_body( input, lang )
+    body = ''
+    line = input.readline
+    delim = /^\.\.#{lang}$/
+    until delim =~ line do
+      body << line
+      line = input.readline
+    end
+    if lang == 'ja' then
+      @body_ja = body.strip
+    else
+      @body_en = body.strip
+    end
+  end
+
+  def to_div_en
+    return '' unless @subject_en
+    ret = "<div><h2><a name='#{@uid}'>#{@subject_en} (#{@date})</a></h2>\n"
+    ret << "<p>#{@body_en}</p></div>"
+    return ret
+  end
+
+  def to_div_ja
+    return '' unless @subject_ja
+    ret = "<div><h2><a name='#{@uid}'>#{@subject_ja} (#{@date})</a></h2>\n"
+    ret << "<p>#{@body_ja}</p></div>"
+    return ret
+  end
+
+end 				# class NewsArticle
--- docsrc/make_html.rb
+++ docsrc/make_html.rb
@@ -1,52 +1,68 @@
 #!/usr/bin/env ruby
 require 'docutils'
 
-if ARGV.size != 2 then
-  $stderr.puts "usage: ruby make_html.rb SRCFILE DSTDIR"
+# parse options
+unless (2..3) === ARGV.size then
+  $stderr.puts "usage: ruby make_html.rb SRCFILE DSTDIR [NEWS?]"
   exit 1
 end
-
 SRCFILE = ARGV.shift
 DSTDIR = ARGV.shift
+NEWS_P = (ARGV.shift == "news")
 
-def rd_to_html(src, dst, lang, charset = nil)
-  unless charset then
-    charset = case lang
-	      when 'en' then "us-ascii"
-	      when 'ja' then "euc-jp"
-	      end
-  end
+def src_to_html(src, dst, lang, fmt)
+  charset =
+    if lang == 'ja' then
+      'euc-jp'
+    else
+      lang = 'en'
+      'us-ascii'
+    end
+
   $stderr.print "#{src}..." ; $stderr.flush
   tmpl = PageTemplate.new( 'page-template.html' )
 
   # setup embeding parts. 
-  [
+  embeding_parts = [
     ["site-menubar.#{lang}.xml", 'site-menubar' ],
-    ["sidebar-news.#{lang}.xml", 'sidebar-news' ],
     ["sidebar-contents.#{lang}.xml", 'sidebar-contents' ],
     ["sidebar-links.#{lang}.xml", 'sidebar-links' ],
+  ]
+  embeding_parts.push ["sidebar-news.#{lang}.xml", 'sidebar-news' ] if NEWS_P
 
-  ].each do |fname, idval|
+  embeding_parts.each do |fname, idval|
     xmlsrc = File.open(fname){|f|f.read}
     xmldoc = REXML::Document.new( xmlsrc )
     tmpl.replace_elements( 'id', idval, xmldoc.root )
   end
 
-  rdoc = RDDocument.new(src)
-  tmpl.set_title( rdoc.title )
-  tmpl.set_contents( rdoc.body )
-  tmpl.set_lang( lang )
-  tmpl.write_to_file( dst )
+  if fmt == 'rd' then
+    rdoc = RDDocument.new( src, lang, charset )
+    tmpl.set_title( rdoc.title )
+    tmpl.set_contents( rdoc.body )
+    tmpl.set_lang( lang )
+    tmpl.write_to_file( dst )
+  elsif fmt == 'xml' then
+    xmldata = REXML::Document.new( File.open(src) )
+    title = xmldata.root.elements['h1'].text
+    tmpl.set_title( Uconv.u8toeuc(title) )
+    tmpl.set_contents( xmldata.root )
+    tmpl.set_lang( lang )
+    tmpl.write_to_file( dst )
+  else
+    raise "unknwon document format '#{fmt}'"
+  end
   $stderr.puts "done."
 end
+
 
 docs = [ SRCFILE ]
 
 docs.each do |path|
   dirname = File.dirname(path)
   filename = File.basename(path)
-  docname = filename.split('.')[0..-2].join
-  lang = filename.split('.')[-1]
+  lang, fmt = filename.split('.')[-2, 2]
+  docname = filename.split('.')[0..-3].join('.')
   dstpath = "#{DSTDIR}/#{docname.downcase}.#{lang}.html"
-  rd_to_html(path, dstpath, lang)
+  src_to_html(path, dstpath, lang, fmt)
 end
