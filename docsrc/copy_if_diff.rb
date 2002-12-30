4aad0bc2e66c826cc85a74dcfce1ba2d717a773e
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2002-12-30 00:27:31 +0000

    wheninstallation process, check diff

---
+++ docsrc/copy_if_diff.rb
@@ -0,0 +1,28 @@
+# parse arg
+
+def usage
+  $stderr.puts "usage: ruby install_html.rb FILE... TARGET_DIR"
+  exit 1
+end
+
+# parse args
+SRCS = ARGV[0..-2]
+TARGET = ARGV[-1]
+
+# check arg
+usage unless FileTest.directory?( TARGET )
+SRCS.each {|src| usage unless FileTest.file?( src ) }
+
+# diff ignore pattern
+PAT_1 = '\$Date:.*\$'
+PAT_2 = '\$Revision:.*\$'
+
+# process
+SRCS.each do |src|
+  dstpath = File.join( TARGET, File.basename( src ) )
+  cmdstr = "diff --brief -u -I '#{PAT_1}' -I '#{PAT_2}' #{src} #{dstpath}"
+  unless system( cmdstr )  then
+    cmdstr = "cp -f -p #{src} #{dstpath}"
+    $stderr.puts( cmdstr ) if system( cmdstr )
+  end
+end
