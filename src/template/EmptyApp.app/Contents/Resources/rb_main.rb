52b0b1ead6d205683ed09be9d4e23baa808c8bd5
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2003-01-16 02:27:53 +0000

    fix 'require' bug of rb_main_init in every rb_main.rb

--- src/template/EmptyApp.app/Contents/Resources/rb_main.rb
+++ src/template/EmptyApp.app/Contents/Resources/rb_main.rb
@@ -7,7 +7,9 @@ require 'osx/cocoa'
 def rb_main_init
   path = OSX::NSBundle.mainBundle.resourcePath.to_s
   rbfiles = Dir.glob(File.join(path, '*.rb')) - [ __FILE__ ]
-  rbfiles.each {|path| require(path) }
+  rbfiles.each do |path|
+    require( File.basename(path) )
+  end
 end
 
 if $0 == __FILE__ then
