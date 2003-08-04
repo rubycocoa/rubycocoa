6ca49506e100ef20940055e8fe67d38cdf555890
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2003-08-04 12:55:08 +0000

    specify false to parameter of Module#instance_methods

--- src/install.rb
+++ src/install.rb
@@ -17,7 +17,7 @@
 
 ### begin compat.rb
 
-unless Enumerable.instance_methods.include? 'inject' then
+unless Enumerable.instance_methods(false).include? 'inject' then
 module Enumerable
   def inject( result )
     each do |i|
