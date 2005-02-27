6c2aacd331848066f412e5374d94b5b9727e9ec1
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2005-02-27 07:54:47 +0000

    *** empty log message ***

--- src/sample/RubyRaiseMan/rb_main.rb
+++ src/sample/RubyRaiseMan/rb_main.rb
@@ -5,7 +5,7 @@
 require 'osx/cocoa'
 
 def rb_main_init
-  path = OSX::NSBundle.mainBundle.resourcePath.to_s
+  path = OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation
   rbfiles = Dir.entries(path).select {|x| /\.rb\z/ =~ x}
   rbfiles -= [ File.basename(__FILE__) ]
   rbfiles.each do |path|
