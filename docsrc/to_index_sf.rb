1a93a0fed8594652febce257b513be573756e4ff
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2005-03-25 23:17:55 +0000

    primary website was moved to sf.net

--- docsrc/to_index_sf.rb
+++ docsrc/to_index_sf.rb
@@ -4,7 +4,7 @@ def replace_link(elm)
   if elm.name == 'a' then
     href = elm.attributes['href']
     if /\Ahttp:/ !~ href and /\Amailto:/ !~ href and /\.html(#\d+)?\z/ =~ href then
-      elm.attributes['href'] = "http://www.fobj.com/rubycocoa/doc/#{href}"
+      elm.attributes['href'] = "http://rubycocoa.sourceforge.net/doc/#{href}"
     end
   end
   elm.each_element do |child|
