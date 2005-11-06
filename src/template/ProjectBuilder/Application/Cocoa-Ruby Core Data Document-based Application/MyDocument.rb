1bd53ca65f2d3cabcfc08fd3d7310818061fcb6a
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2005-11-06 02:15:16 +0000

    call OSX::CoreData.define_wrapper at initializing a document.

--- src/template/ProjectBuilder/Application/Cocoa-Ruby Core Data Document-based Application/MyDocument.rb
+++ src/template/ProjectBuilder/Application/Cocoa-Ruby Core Data Document-based Application/MyDocument.rb
@@ -8,10 +8,12 @@
 
 
 require 'osx/cocoa'
+require 'osx/coredata'
 
 class MyDocument < OSX:: NSPersistentDocument
 
-  ns_overrides 'windowNibName', 'windowControllerDidLoadNib:'
+  ns_overrides 'windowNibName', 'windowControllerDidLoadNib:',
+    'setManagedObjectContext:'
 
   def windowNibName
     return "MyDocument"
@@ -20,6 +22,12 @@ class MyDocument < OSX:: NSPersistentDocument
   def windowControllerDidLoadNib (aController)
     super_windowControllerDidLoadNib(aController)
     # user interface preparation code
+  end
+
+  # define accessors for properties of models
+  def setManagedObjectContext(context)
+    super_setManagedObjectContext(context)
+    OSX::CoreData.define_wrapper(managedObjectModel)
   end
 
 end
