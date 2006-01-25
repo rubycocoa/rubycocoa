1cbbef0099b1c731101a00ec2c5d9364f3ddf325
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2006-01-25 14:10:33 +0000

    add test for OCObjcWrapper

---
+++ src/tests/tc_wrapper.rb
@@ -0,0 +1,47 @@
+#
+#  $Id$
+#
+#  Copyright (c) 2006 FUJIMOTO Hisakuni <hisa@imasy.or.jp>
+#
+#  This program is free software.
+#  You can distribute/modify this program under the terms of
+#  the GNU Lesser General Public License version 2.
+#
+
+require 'test/unit'
+require 'osx/cocoa'
+
+class TC_OCObjWrapper < Test::Unit::TestCase
+  include OSX
+
+  def setup
+    @obj = NSObject.alloc.init
+    @data = NSData.alloc.init
+  end
+
+  def teardown
+  end
+
+  def test_instance_methods
+    assert_equal_array(@obj.objc_methods, NSObject.objc_instance_methods)
+    assert_equal_array(@obj.objc_methods, @obj.objc_instance_methods)
+  end
+
+  def test_instance_methods_nonrecursive
+    assert_equal_array(NSObject.objc_instance_methods | NSData.objc_instance_methods(false), NSData.objc_instance_methods(true))
+  end
+
+  def test_class_methods
+    assert_equal_array(NSObject.objc_methods, NSObject.objc_class_methods)
+    assert_equal_array(NSObject.objc_methods, @obj.objc_class_methods)
+  end
+
+  def test_class_methods_nonrecursive
+    assert_equal_array(NSObject.objc_class_methods | NSData.objc_class_methods(false), NSData.objc_class_methods(true))
+  end
+
+  def assert_equal_array(ary1, ary2)
+    assert_equal((ary1 | ary2).size, ary1.size)
+  end
+
+end
