b1b8c2e0da1fb37fc8db13d38a02d0a207c56af1
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2006-01-29 12:27:55 +0000

    revert ownership of working files

--- src/package/post-package.rb
+++ src/package/post-package.rb
@@ -5,7 +5,6 @@ contents_dir = File.join(work_dir, 'files')
 resources_dir = File.join(work_dir, 'resources')
 
 package_name = "RubyCocoa-#{@config['rubycocoa-version']}-OSX#{@config['macosx-deployment-target']}"
-package_name = package_name.sub(/snapshot-/, '')
 if @config['build-universal'] == 'yes'
   package_name << 'universal'
 end
@@ -21,6 +20,9 @@ command %Q!"#{pkgmaker}" -build -v ! +
 	%Q!-f "#{contents_dir}" -r "#{resources_dir}" ! +
 	%Q!-i "#{File.join(work_dir, 'Info.plist')}" ! +
 	%Q!-d "#{File.join(work_dir, 'Description.plist')}" !
+
+# revert owner 
+command "sudo chown -R `/usr/bin/id -un` \"#{contents_dir}\""
 
 save_dir = Dir.pwd
 Dir.chdir dmg_dir
