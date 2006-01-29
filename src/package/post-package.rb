b2e6b74f25f32a3e8e8f63535bdb99fd44fce339
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2006-01-29 08:58:09 +0000

    add package/

---
+++ src/package/post-package.rb
@@ -0,0 +1,35 @@
+# $Id$
+
+work_dir = 'work'
+contents_dir = File.join(work_dir, 'files')
+resources_dir = File.join(work_dir, 'resources')
+
+package_name = "RubyCocoa-#{@config['rubycocoa-version']}-OSX#{@config['macosx-deployment-target']}"
+if @config['build-universal'] == 'yes'
+  package_name << 'universal'
+end
+dmg_dir = File.join(work_dir, package_name)
+Dir.mkdir dmg_dir
+
+pkgmaker = '/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker'
+
+command %Q!"#{pkgmaker}" -build -v ! +
+	%Q!-p "#{File.join(dmg_dir, package_name)}.pkg" ! +
+	%Q!-f "#{contents_dir}" -r "#{resources_dir}" ! +
+	%Q!-i "#{File.join(work_dir, 'Info.plist')}" ! +
+	%Q!-d "#{File.join(work_dir, 'Description.plist')}" !
+
+save_dir = Dir.pwd
+Dir.chdir dmg_dir
+resources = File.join(package_name + '.pkg', 'Contents', 'Resources')
+File.symlink(File.join(resources, 'License.txt'), 'License.txt')
+File.symlink(File.join(resources, 'English.lproj', 'ReadMe.html'), 'ReadMe.html')
+File.symlink(File.join(resources, 'Japanese.lproj', 'ReadMe.html'), 'ReadMe.ja.html')
+Dir.chdir save_dir
+
+command %Q!/usr/bin/hdiutil create ! +
+	%Q!-srcfolder "#{dmg_dir}" ! +
+	%Q!-format UDZO -tgtimagekey zlib-level=9 ! +
+	%Q!-fs HFS+ -volname "#{package_name}" ! +
+	%Q!"#{File.join(work_dir, package_name)}.dmg" !
+
