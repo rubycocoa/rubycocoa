bd62afdd3429ceb1d389860ebc4a08d82cb609e9
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2006-05-16 13:13:09 +0000

    add tests for retainCount

--- src/tests/objc_test.m
+++ src/tests/objc_test.m
@@ -34,6 +34,45 @@
 }
 @end
 
+@interface RetainCount : NSObject
+{
+}
+@end
+
+@implementation RetainCount
+
++(int) rbAllocCount
+{
+  int retainCount;
+  id obj;
+  obj = [NSClassFromString(@"RBSubclass") alloc];
+  retainCount = [obj retainCount];
+  [obj release];
+  return retainCount;
+}
+
++(int) rbInitCount
+{
+  int retainCount;
+  id obj;
+  obj = [[NSClassFromString(@"RBSubclass") alloc] init];
+  retainCount = [obj retainCount];
+  [obj release];
+  return retainCount;
+}
+
++(id) ocObject
+{
+  return [[NSObject alloc] init];
+}
+
++(id) rbObject
+{
+  return [[NSClassFromString(@"RBSubclass") alloc] init];
+}
+
+@end
+
 void Init_objc_test(){
   // dummy initializer for ruby's `require'
 }
---
+++ src/tests/tc_retaincount.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require 'osx/cocoa'
+
+system 'make' || raise(RuntimeError, "'make' failed")
+require 'objc_test.bundle'
+
+OSX.ns_import :RetainCount
+
+class RBSubclass < OSX::NSObject
+end
+
+class TC_RetainCount < Test::Unit::TestCase
+
+  # retained by Ruby
+  def test_rb_rbclass
+    assert_equal(1, RBSubclass.alloc.retainCount, 'alloc')
+# FIXME 
+#    assert_equal(1, RBSubclass.allocWithZone(nil).retainCount, 'allocWithZone:')
+    assert_equal(1, RBSubclass.alloc.init.retainCount, 'alloc.init')
+  end
+
+  # retained by Ruby
+  def test_rb_occlass
+    assert_equal(1, OSX::NSObject.alloc.retainCount, 'alloc')
+    assert_equal(1, OSX::NSObject.alloc.init.retainCount, 'alloc.init')
+    assert_equal(1, OSX::NSString.stringWithString('foo').retainCount, 'factory')
+  end
+
+  # retained by Objective-C
+  def test_objc_rbclass
+    assert_equal(1, OSX::RetainCount.rbAllocCount, 'alloc')
+    assert_equal(1, OSX::RetainCount.rbInitCount, 'alloc.init') 
+  end
+
+  # retained by Ruby and Objective-C
+  def test_obj_from_objc
+    assert_equal(2, OSX::RetainCount.ocObject.retainCount, 'ObjC object')
+    assert_equal(2, OSX::RetainCount.rbObject.retainCount, 'Ruby object')
+  end
+
+end
+
