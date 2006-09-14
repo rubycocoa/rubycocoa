901dd891484bb6dfc29705d5f9d5cbec083179ba
lrz <lrz@f4f36d40-9719-0410-9868-dde021a23eda>
2006-09-14 10:53:37 +0000

    new test case

---
+++ src/tests/tc_booleans.rb
@@ -0,0 +1,19 @@
+#
+#  Copyright (c) 2006 Laurent Sansonetti, Apple Computer Inc.
+#
+
+require 'test/unit'
+require 'osx/cocoa'
+
+class TC_Booleans < Test::Unit::TestCase
+
+    def test_boolean_conversion
+        s1 = OSX::NSString.alloc.initWithString("foo")
+        s2 = s1.copy
+        s3 = OSX::NSString.alloc.initWithString("bar")
+        assert_equal(true, s1.isEqualToString(s2))
+        assert_equal(true, s1.isEqualToString?(s2))
+        assert_equal(false, s1.isEqualToString(s3))
+        assert_equal(false, s1.isEqualToString?(s3))
+    end
+end
