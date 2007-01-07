1d01c9526fe79398b2e8d189f1d87fc68b9f58db
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2007-01-07 00:33:25 +0000

    support to multiple language text

--- htdocs/doc/unstable/gen.rb
+++ htdocs/doc/unstable/gen.rb
@@ -2,19 +2,23 @@ require 'rubygems'
 require 'redcloth'
 require 'syntax/convertors/html'
 
-HEADER = <<EOS
+def header(lang)
+  <<"EOS"
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html lang="en-US">
+<html lang="#{lang}">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Language" content="en-US">
+  <meta http-equiv="Content-Language" content="#{lang}">
   <link rel="stylesheet" type="text/css" href="style.css">
   <title>RubyCocoa 1.0 Sneak Preview</title>
 </head>
 <body>
 EOS
+end
 
 Dir.glob("*.txt").each do |txt|
+    ma = /-(.*)\.txt$/.match(txt)
+    lang = ma ? ma[1] : 'en-US'
     html = txt.sub(/txt$/, 'html')
     r = RedCloth.new(IO.read(txt))
     File.open(html, 'w') do |io|
@@ -26,7 +30,7 @@ Dir.glob("*.txt").each do |txt|
       end
 =end
       h = r.to_html
-      io.write(HEADER)
+      io.write(header(lang))
       io.write(h)
     end
 end
