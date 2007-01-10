87ef2f90f74f6479e2481517da2a994b51fe3ae5
lrz <lrz@f4f36d40-9719-0410-9868-dde021a23eda>
2007-01-10 18:16:01 +0000

    load CoreGraphics before AppKit

--- src/framework/src/ruby/osx/objc/cocoa.rb
+++ src/framework/src/ruby/osx/objc/cocoa.rb
@@ -6,8 +6,8 @@
 
 require 'osx/objc/foundation'
 
-OSX.require_framework('AppKit')
 OSX.require_framework('CoreGraphics')
+OSX.require_framework('AppKit')
 
 require 'osx/objc/oc_types_appkit'
 require 'osx/objc/oc_attachments_appkit'
