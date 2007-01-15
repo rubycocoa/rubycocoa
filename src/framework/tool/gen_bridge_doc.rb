033a558753a1bb1ec50354322586f9750bbabf47
el_oy <el_oy@f4f36d40-9719-0410-9868-dde021a23eda>
2007-01-15 15:34:18 +0000

    2007-01-15  Eloy Duran <eloy.de.enige@gmail.com>
      * framework/tool/gen_bridge_doc.rb
      Updated this script so that it also accepts the verbose and force flags: -v, -f

--- src/framework/tool/gen_bridge_doc.rb
+++ src/framework/tool/gen_bridge_doc.rb
@@ -3,16 +3,45 @@
 # Created by Eloy Duran
 # Copyright (c) 2007 Eloy Duran, SuperAlloy
 
+def show_options
+  puts "Usage:"
+  puts "  #{__FILE__} build|clean [extra options]"
+  puts ""
+  puts "Options:"
+  puts "  build  Create the RDoc documentation for the supported frameworks"
+  puts "  clean  Remove all the files created by this tool"
+  puts ''
+  puts "Extra Options:"
+  puts "  The extra options only apply to 'build' option and are options passed"
+  puts "  to the actual rdocify_framework.rb script."
+  puts "  These are:"
+  puts "  -v Verbose output."
+  puts "  -f Force the output files to be written even if there were errors during parsing."
+  puts ''
+  puts "Examples:"
+  puts "  #{__FILE__} build"
+  puts "  #{__FILE__} build -v -f"
+  puts "  #{__FILE__} clean"
+  puts ''
+end
+
 unless ARGV[0].nil?
   case ARGV[0]
   when 'build'
+    # Check if there are any other args
+    if ARGV.length > 1
+      other_args = ARGV[1..-1]
+    else
+      other_args = []
+    end
+    
     SUPPORTED_FRAMEWORKS = ['/Developer/ADC Reference Library/documentation/Cocoa/Reference/ApplicationKit/']
 
     start_time = Time.now
 
     # Parse the rdoc for each supported framework
     SUPPORTED_FRAMEWORKS.each do |f|
-      system "ruby gen_bridge_doc/rdocify_framework.rb '#{f}'"
+      system "ruby gen_bridge_doc/rdocify_framework.rb #{other_args.join(' ')} '#{f}'"
     end
 
     # Create the rdoc files
@@ -24,20 +53,8 @@ unless ARGV[0].nil?
     system "rm -r doc"
     system "rm -r gen_bridge_doc/output"
   else
-    puts "Usage:"
-    puts "  #{__FILE__} [build|clean]"
-    puts ""
-    puts "Options:"
-    puts "  build  Create the RDoc documentation for the supported frameworks"
-    puts "  clean  Remove all the files created by this tool"
-    puts ''
+    show_options
   end
 else
-  puts "Usage:"
-  puts "  #{__FILE__} [build|clean]"
-  puts ""
-  puts "Options:"
-  puts "  build  Create the RDoc documentation for the supported frameworks"
-  puts "  clean  Remove all the files created by this tool"
-  puts ''
+  show_options
 end
