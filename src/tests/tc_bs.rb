52ef54b2cbd461c7b0bc6341c876d345760c66cc
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2007-01-19 23:17:34 +0000

    * tests/tc_bs.rb: Fixed test to spawn Ruby command lines to add option for seeking library path for ../lib and ../ext/rubycocoa, to pass the test even if any RubyCocoa isn't installed.

--- src/tests/tc_bs.rb
+++ src/tests/tc_bs.rb
@@ -33,7 +33,7 @@ class TC_BridgeSupport < Test::Unit::TestCase
   end
 
   def __spawn_line(line)
-    res = `RUBYLIB=../lib DYLD_FRAMEWORK_PATH=../framework/build/Default #{@ruby_path} -e \"#{line}\"`
+    res = `DYLD_FRAMEWORK_PATH=../framework/build/Default #{@ruby_path} -I../lib -I../ext/rubycocoa -e \"#{line}\"`
     raise "Can't spawn Ruby line: '#{line}'" unless $?.success?
     return res.strip
   end
