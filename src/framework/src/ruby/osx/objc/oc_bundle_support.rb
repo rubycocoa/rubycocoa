175c2d512121a430055ab35ca323e46901e1cb8c
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2007-02-07 06:08:43 +0000

    2007-02-07  Fujimoto Hisa  <hisa@fobj.com>
    	* bundle-support branch - temporary branch (from apple-unstable)
    	for *EXPERIMENT* of bundle/plugin support. Maybe these API/SPEC is
    	dynamically changed for a while.
    
    	* framework/RubyCocoa.xcodeproj/project.pbxproj,
    	  framework/src/ruby/osx/objc/oc_bundle_support.rb,
    	  framework/src/ruby/osx/objc/oc_all.rb
    	  framework/src/ruby/osx/objc/oc_import.rb:
    	temporary implementation for bundle/plugin support by ruby program.
    
    	* sample/RubyAnywhere:
    	SIMBL plugin example.
    
    	* sample/VPRubyPluginEnabler:
    	VoodooPad plugin example (not complete)

---
+++ src/framework/src/ruby/osx/objc/oc_bundle_support.rb
@@ -0,0 +1,59 @@
+# -*- mode:ruby; indent-tabs-mode:nil -*-
+#
+#  oc_bundle_support.rb
+#  RubyCocoa
+#  $Id$
+#
+#  Copyright (c) 2007 FUJIMOTO Hisakuni
+
+module OSX
+
+  if not NSBundle.respondsToSelector?('_originalBundleForClass:') then
+    NSBundle.objc_alias_class_method '_originalBundleForClass:', 'bundleForClass:'
+    class NSBundle
+      def NSBundle.bundleForClass(cls)
+        OSX::NSClassFromString("NSObject")
+        bdl = OSX::BundleSupport.bundle_for_class(cls)
+        bdl ||= self._originalBundleForClass(cls)
+        return bdl
+      end
+    end
+    NSBundle.objc_class_method 'bundleForClass:', [:id, :class]
+  end
+
+  module BundleSupport
+    @@bundles = []
+    @@bundle_map = {}
+
+    def do_with_bundle(bdl)
+      _push_bundle!(bdl)
+      yield(bdl)
+    ensure
+      _pop_bundle!
+    end
+    module_function :do_with_bundle
+
+    def do_for_class(cls)
+      do_with_bundle(OSX::NSBundle._originalBundleForClass(cls)) {|b| yield(b) }
+    end
+    module_function :do_for_class
+
+    def bundle_for_class(cls)
+      @@bundle_map[cls.__ocid__]
+    end
+    module_function :bundle_for_class
+
+    def bind_class_with_current_bundle(cls)
+      if bdl = _current_bundle then
+        @@bundle_map[cls.__ocid__] = bdl
+      end
+    end
+    module_function :bind_class_with_current_bundle
+
+    private
+    def self._push_bundle!(bdl) @@bundles.push(bdl) end
+    def self._pop_bundle!;      @@bundles.pop  end
+    def self._current_bundle;   @@bundles.last end
+  end
+
+end
--- src/framework/src/ruby/osx/objc/oc_import.rb
+++ src/framework/src/ruby/osx/objc/oc_import.rb
@@ -246,6 +246,7 @@ module OSX
       kls_name = self.name.split('::')[-1]
       occls = OSX.objc_derived_class_new(self, kls_name, spr_name)
       self.instance_eval "@ocid = #{occls.__ocid__}",__FILE__,__LINE__+1
+      OSX::BundleSupport.bind_class_with_current_bundle(self) if kls_name
       @inherited = true
     end
 
