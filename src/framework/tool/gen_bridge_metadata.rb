8768f9d0f0bb60134685f935877300df8913dc52
lrz <lrz@f4f36d40-9719-0410-9868-dde021a23eda>
2007-02-14 10:35:16 +0000

    2007-02-14  Laurent Sansonetti <lsansonetti@apple.com>
      * framework/tool/gen_bridge_metadata.rb:
      Honors the CFLAGS environment variable when generating the dylib.

--- src/framework/tool/gen_bridge_metadata.rb
+++ src/framework/tool/gen_bridge_metadata.rb
@@ -656,7 +656,7 @@ EOC
     if File.extname(@out_file) != '.dylib'
       @out_file << '.dylib'
     end
-    gcc = "gcc #{tmp_src.path} -o #{@out_file} #{@compiler_flags} -dynamiclib -O3"
+    gcc = "gcc #{ENV['CFLAGS']} #{tmp_src.path} -o #{@out_file} #{@compiler_flags} -dynamiclib -O3"
     unless system(gcc)
       die "Can't compile dylib source file '#{tmp_src.path}'"
     end
