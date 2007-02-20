667aebe931e59109579d3884b0a537aa8507d4e2
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2007-02-20 07:50:29 +0000

    2007-02-20  Fujimoto Hisa  <hisa@fobj.com>
    	* sample/VPRubyPluginEnabler:
    	aware of the standard user's "Script PlugIns" folder for ruby
    	script. implemented "Run Page as Ruby Plugin" and "Save Page as
    	Ruby Plugin..." command same as Lua version.

--- src/sample/VPRubyPluginEnabler/VPRubyScript.rb
+++ src/sample/VPRubyPluginEnabler/VPRubyScript.rb
@@ -6,22 +6,44 @@
 #  Created by Fujimoto Hisa on 07/02/09.
 #  Copyright (c) 2007 FOBJ SYSTEMS. All rights reserved.
 
+# typical script example...
+#
+#   # the menu item specs
+#   vp_spec( :menuTitle      => 'do something', # required
+#            :superMenuTitle => 'category',     # optional
+#            :shortcutKey    => 'j',            # optional
+#            :shortcutMask   => [ :shift ] )    # optional (see SHORTCUT_MASKS)
+#
+#   # the program for the script
+#   vp_action do |window_controller|
+#     ...
+#     OSX.NSRunAlertPanel(window_controller.window.title, "OK", nil, nil, nil)
+#     ...
+#   end
+
 require 'osx/cocoa'
 
 class VPRubyScript < OSX::NSObject
 
+  # primitive: vp_spec - describe the menu item specs for the script
+  def vp_spec(spec)
+    @spec = spec
+  end
+
+  # primitive: vp_action - descript the action/procedure for the script
+  def vp_action(&blk)
+    @action = blk
+  end
+
+  # syntax-sugar: vp_script - combination of vp_spec and vp_action
+  def vp_script(spec, &blk)
+    @spec   = spec
+    @action = blk
+  end
+
+  # utility: loginfo, logerror - logging wrapper for NSLog
   def loginfo(fmt, *args) VPRubyPlugin.loginfo(fmt, *args) end
   def logerror(err)       VPRubyPlugin.logerror(err)       end
-
-  def self.load(path)
-    return self.alloc.initWithPath(path)
-  end
-
-  attr_reader :selector
-
-  %w( menuTitle superMenuTitle shortcutKey ).each do |key|
-    define_method(key) { @spec[key.to_sym] }
-  end
 
   SHORTCUT_MASKS = {
     :alpha_shift => OSX::NSAlphaShiftKeyMask,
@@ -33,6 +55,27 @@ class VPRubyScript < OSX::NSObject
     :numeric_pad => OSX::NSNumericPadKeyMask,
     :shift       => OSX::NSShiftKeyMask,
   }
+
+  def VPRubyScript.create(spec = nil, &blk)
+    script = VPRubyScript.alloc.init
+    script.vp_spec(spec)   if spec
+    script.vp_action(&blk) if blk
+    return script
+  end
+
+  def VPRubyScript.load(path)
+    return VPRubyScript.alloc.initWithPath(path)
+  end
+
+  def VPRubyScript.parse(src)
+    return VPRubyScript.alloc.initWithSource(src)
+  end
+
+  attr_reader   :selector
+
+  %w( menuTitle superMenuTitle shortcutKey ).each do |key|
+    define_method(key) { @spec[key.to_sym] }
+  end
 
   def shortcutMask
     mask = @spec[:shortcutMask]
@@ -47,38 +90,59 @@ class VPRubyScript < OSX::NSObject
     return mask.to_i
   end
 
-  def initWithPath(path)
+  def init
     @selector = 'execute:'
+    @spec   = {}
+    @action = nil
+    @path = @source = nil
+    return self
+  end
+
+  def initWithPath(path)
+    self.init
     @path = path
+    @source = File.read(@path)
+    parse!
+    return self
+  end
+
+  def initWithSource(src)
+    self.init
+    @source = src
     parse!
     return self
   end
 
   def execute(windowController)
     parse!
-    @procedure.call(windowController)
+    @action.call(windowController) if @action
   rescue Exception => err
     logerror(err)
   end
   objc_method :execute, [:void, :id]
 
+  def install_menu(manager)
+    if self.menuTitle then
+      manager.objc_send( :addPluginsMenuTitle, self.menuTitle,
+                          :withSuperMenuTitle, self.superMenuTitle,
+                                      :target, self,
+                                      :action, self.selector,
+                               :keyEquivalent, self.shortcutKey,
+                   :keyEquivalentModifierMask, self.shortcutMask )
+    end
+  end
+
   private
 
   def parse!
-    prog = File.read(@path)
-    instance_eval(prog, @path, 1) # => invoke v_pscript
-  end
-
-  def vp_spec(spec)
-    @spec = spec
-  end
-
-  def vp_action(&blk)
-    @procedure = blk
-  end
-
-  def vp_script(spec, &blk)
-    @spec = spec
-    @procedure = blk
+    name = @path || self.to_s
+    if @path then
+      @source = File.read(@path) 
+      @spec   = {}
+      @action = nil
+    end
+    if @source && @action.nil? then
+      instance_eval(@source, name, 1) # => invoke v_pscript
+    end
   end
 end
--- src/sample/VPRubyPluginEnabler/vpr_init.rb
+++ src/sample/VPRubyPluginEnabler/vpr_init.rb
@@ -15,8 +15,8 @@ OSX.init_for_bundle do
   # bdl     - the bundle related with the 2nd argument of RBBundleInit
   # enabler - the 3rd argument of RBBundleInit (VPRubyPluginEnabler)
   # log     - logger for this block
-  log.info("enabler=%p", enabler.to_s)
-  log.info("enabler.pluginManager=%p", enabler.pluginManager.to_s)
+  # log.info("enabler=%p", enabler.to_s)
+  # log.info("enabler.pluginManager=%p", enabler.pluginManager.to_s)
 
   require 'VPRubyPlugin'
   VPRubyPlugin.logger = log
