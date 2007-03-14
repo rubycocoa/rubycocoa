321d732382c610f761d52a588a6edbe6c658d666
hisa <hisa@f4f36d40-9719-0410-9868-dde021a23eda>
2007-03-14 04:45:56 +0000

    Added SimpleDecorator

---
+++ hisa/junk/CocoaRepl/simple_decorator.rb
@@ -0,0 +1,54 @@
+require 'decorator'
+require 'strscan'
+
+class SimpleStyle < DecoratorStyle
+  install :simple
+  font_size 16
+  style :keyword, :color => :blueColor, :bold   => true
+end
+
+class SimpleDecorator < Decorator
+  install :simple
+  
+  def SimpleDecorator.instance
+    @instance = new if not defined? @instance
+    @instance
+  end
+  
+  def initialize(style = :simple)
+    super(style)
+  end
+  
+  private
+  
+  BLOCK_BEGIN = %w( begin class module do def for )
+  
+  PATTERN_BLOCK_BEGIN = /\b(#{BLOCK_BEGIN.join('|')})\b/u #
+  PATTERN_BLOCK_END   = /\bend\b/u
+  PATTERN_SPACE       = /\s+/u
+  
+  def each_token(source)
+    scanner = StringScanner.new(source)
+    
+    while not scanner.eos? do
+      
+      if scanner.match?(PATTERN_SPACE) then
+        val = scanner.scan(PATTERN_SPACE)
+        token = Decorator::Token.new(:misc, val)
+        
+      elsif scanner.match?(PATTERN_BLOCK_BEGIN) then
+        val = scanner.scan(PATTERN_BLOCK_BEGIN)
+        token = Decorator::Token.new(:keyword, val)
+        
+      elsif scanner.match?(PATTERN_BLOCK_END) then
+        val = scanner.scan(PATTERN_BLOCK_END)
+        token = Decorator::Token.new(:keyword, val)
+        
+      else
+        val = scanner.getch
+        token = Decorator::Token.new(:misc, val)
+      end
+      yield(token)
+    end
+  end
+end
