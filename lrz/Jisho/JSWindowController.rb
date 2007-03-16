1b8e8f8861472f66ed7136dabcffa125d8b15fa4
lrz <lrz@f4f36d40-9719-0410-9868-dde021a23eda>
2007-03-16 21:22:26 +0000

    ready to be released

--- lrz/Jisho/JSWindowController.rb
+++ lrz/Jisho/JSWindowController.rb
@@ -1,10 +1,22 @@
+# Copyright (c) 2006-2007 Laurent Sansonetti <lrz@chopine.be>
 #
-#  JSWindowController.rb
-#  Jisho
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
 #
-#  Created by Laurent Sansonetti on 11/24/06.
-#  Copyright (c) 2006 Apple Computer. All rights reserved.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 class JSWindowController < NSWindowController
   ib_outlets :textView, :searchField, :fontSizeSegmentedControl, :historySegmentedControl
@@ -28,17 +40,20 @@ class JSWindowController < NSWindowController
     searchText(sender.stringValue.to_s.strip)
   end
   
+  TAG_FONT_SMALLER, TAG_FONT_NORMAL, TAG_FONT_BIGGER = 40, 41, 42
+  TAG_HISTORY_BACK, TAG_HISTORY_FORWARD = 50, 51
   FONT_SIZE_STEP = 2
+
   def updateFontSize(sender)
     tag = sender.is_a?(NSSegmentedControl) ? sender.cell.tagForSegment(sender.selectedSegment) : sender.tag
     vals = case tag
-    when 40
+    when TAG_FONT_SMALLER
       @fontSizeDelta -= FONT_SIZE_STEP
       [@fontSizeDelta > -4, true]
-    when 41
+    when TAG_FONT_NORMAL
       @fontSizeDelta = 0
       [true, true]
-    when 42
+    when TAG_FONT_BIGGER
       @fontSizeDelta += FONT_SIZE_STEP
       [true, @fontSizeDelta < 10]
     end
@@ -49,33 +64,30 @@ class JSWindowController < NSWindowController
   def goHistory(sender)
     tag = sender.is_a?(NSSegmentedControl) ? sender.cell.tagForSegment(sender.selectedSegment) : sender.tag
     vals = case tag
-    when 50
+    when TAG_HISTORY_BACK
       @historyPos -= 1
       [@historyPos > 0, true]
-    when 51
+    when TAG_HISTORY_FORWARD
       @historyPos += 1
       [true, @historyPos < @history.size]
     end
     vals.each_with_index { |x, i| @historySegmentedControl.cell.setEnabled_forSegment(x, i) }
-    refreshView
+    term = @history[@historyPos]
+    @searchField.setStringValue(term)
+    searchText(term, false)
   end
   
   def validateMenuItem(sender)
     if sender.action == 'updateFontSize:'
       case sender.tag
-      when 40
-        @fontSizeDelta > -4
-      when 41
-        @fontSizeDelta != 0
-      when 42
-        @fontSizeDelta < 10
+      when TAG_FONT_SMALLER: @fontSizeDelta > -4
+      when TAG_FONT_NORMAL: @fontSizeDelta != 0
+      when TAG_FONT_BIGGER: @fontSizeDelta < 10
       end
     elsif sender.action == 'goHistory:'
       case sender.tag
-      when 50
-        (!@history.empty? and @historyPos > 0) 
-      when 51
-        (!@history.empty? and @historyPos < @history.size)
+      when TAG_HISTORY_BACK: canGoBack?
+      when TAG_HISTORY_FORWARD: canGoForward?
       end
     else
       true
@@ -84,10 +96,24 @@ class JSWindowController < NSWindowController
   
   private
   
+  def canGoBack?
+    @history.size > 1 and @historyPos > 0
+  end
+  
+  def canGoForward?
+    !@history.empty? and @historyPos < @history.size - 1
+  end
+  
   def searchText(term, recordHistory=true)
     @results = term == '' ? nil : @database.search(term)
-    @history << @results if recordHistory
+    if recordHistory and !(@results.nil? or @results.empty?)
+      @history << term
+      @historyPos = @history.length - 1
+    end
     refreshView
+    cell = @historySegmentedControl.cell
+    cell.setEnabled_forSegment(canGoBack?, 0)
+    cell.setEnabled_forSegment(canGoForward?, 1)
   end
   
   def refreshView
