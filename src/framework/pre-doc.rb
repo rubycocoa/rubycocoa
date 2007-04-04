d67e99dc676f8595171791afde9ea7e755c95c23
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2007-04-04 16:08:10 +0000

    clean bridge-doc directory when doc task fails.

--- src/framework/pre-doc.rb
+++ src/framework/pre-doc.rb
@@ -1,7 +1,12 @@
 require 'fileutils'
 
 unless File.exist?('bridge-doc')
-  Dir.chdir('tool') do
-    ruby("gen_bridge_doc.rb build ../bridge-doc")
+  begin
+    Dir.chdir('tool') do
+      ruby("gen_bridge_doc.rb build ../bridge-doc")
+    end
+  rescue RuntimeError => e
+    rm_rf 'bridge-doc'
+    raise e
   end
 end
