d11707bc69933a0443e0defeb5007c5c716061f3
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2007-05-23 12:59:28 +0000

    fix: NSApplication.run_with_temp_app does not work.

--- src/framework/src/ruby/osx/objc/cocoa_macros_appkit.rb
+++ src/framework/src/ruby/osx/objc/cocoa_macros_appkit.rb
@@ -5,17 +5,15 @@
 # RubyCocoa is free software, covered under either the Ruby's license or the 
 # LGPL. See the COPYRIGHT file for more information.
 
-require 'nkf'
-
 module OSX
 
   # for NSApplication
   class NSApplication 
     
-    def NSApplication.run_with_temp_app(terminate = true, &proc)
-      # prepare delegate
-      delegate = Class.new(OSX::NSObject).alloc.init
-      def delegate.applicationDidFinishLaunching(sender)
+    class RBCCTemporaryDelegate < OSX::NSObject
+      attr_writer :proc, :terminate
+
+      def applicationDidFinishLaunching(sender)
 	begin
 	  @proc.call
 	rescue Exception => err
@@ -25,12 +23,12 @@ module OSX
 	  OSX::NSApplication.sharedApplication.terminate(self) if @terminate
 	end
       end
-      def delegate.proc=(block)
-	@proc = block
-      end
-      def delegate.terminate=(terminate)
-	@terminate = terminate
-      end
+
+    end
+
+    def NSApplication.run_with_temp_app(terminate = true, &proc)
+      # prepare delegate
+      delegate = RBCCTemporaryDelegate.alloc.init
       delegate.proc = proc
       delegate.terminate = terminate
       # run a new app
