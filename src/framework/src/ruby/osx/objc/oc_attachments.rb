a7954f92bc04bad906bb10b294b599105fceb784
psychs <psychs@f4f36d40-9719-0410-9868-dde021a23eda>
2007-08-27 14:46:57 +0000

    2007-08-27  Satoshi Nakagawa <artension@gmail.com>
      * framework/src/ruby/osx/objc/oc_attachments.rb,
        tests/tc_nsarray.rb:
      - Refactoring Array methods on NSArray.
      - Improved tests.

--- src/framework/src/ruby/osx/objc/oc_attachments.rb
+++ src/framework/src/ruby/osx/objc/oc_attachments.rb
@@ -81,22 +81,22 @@ module OSX
       case args.length
       when 1
         count = self.count
-        case args.first
+        first = args.first
+        case first
         when Numeric
-          index = args.first
-          index = index.to_i
+          index = first.to_i
           index += count if index < 0
-          if (0...count).include?(index)
-            self.objectAtIndex(index)
+          if 0 <= index && index < count
+            objectAtIndex(index)
           else
             nil
           end
         when Range
-          range = OSX::NSRange.new(args.first, count)
-          case range.location
-          when 0...count
+          range = OSX::NSRange.new(first, count)
+          loc = range.location
+          if 0 <= loc && loc < count
             indexset = OSX::NSIndexSet.indexSetWithIndexesInRange(range)
-            result = self.objectsAtIndexes(indexset)
+            result = objectsAtIndexes(indexset)
             result.to_a
           else
             nil
@@ -140,11 +140,10 @@ module OSX
           end
           index = index.to_i
           index += count if index < 0
-          case index
-          when 0...count
-            self.replaceObjectAtIndex_withObject(index, value)
-          when count
-            self.addObject(value)
+          if 0 <= index && index < count
+            replaceObjectAtIndex_withObject(index, value)
+          elsif index == count
+            addObject(value)
           else
             raise IndexError, "index #{args[0]} out of array"
           end
@@ -152,31 +151,31 @@ module OSX
         when Range
           range, value = args
           nsrange = OSX::NSRange.new(range, count)
-          case nsrange.location
-          when 0...count
+          loc = nsrange.location
+          if 0 <= loc && loc < count
             if nsrange.length > 0
-              self.removeObjectsInRange(nsrange)
+              removeObjectsInRange(nsrange)
             end
             value = value.to_a if value.is_a? NSArray
             if value != nil && value != []
               if value.is_a? Array
-                index = nsrange.location
-                value.each {|i| self.insertObject_atIndex(i, index); index += 1 }
+                index = loc
+                value.each {|i| insertObject_atIndex(i, index); index += 1 }
               else
-                self.insertObject_atIndex(value, nsrange.location)
+                insertObject_atIndex(value, loc)
               end
             end
-          when count
+          elsif loc == count
             value = value.to_a if value.is_a? NSArray
             if value != nil && value != []
               if value.is_a? Array
-                self.addObjectsFromArray(value)
+                addObjectsFromArray(value)
               else
-                self.addObject(value)
+                addObject(value)
               end
             end
           else
-            raise IndexError, "index #{args[0].first} out of array"
+            raise IndexError, "index #{loc} out of array"
           end
           value
         else
@@ -204,19 +203,19 @@ module OSX
       end
     end
     
-    def +(other); self.to_a + other; end
-    def *(times); self.to_a * times; end
-    def -(other); self.to_a - other; end
-    def &(other); self.to_a & other; end
-    def |(other); self.to_a | other; end
+    def +(other); to_a + other; end
+    def *(times); to_a * times; end
+    def -(other); to_a - other; end
+    def &(other); to_a & other; end
+    def |(other); to_a | other; end
     
     def <<(obj)
-      self.addObject(obj)
+      addObject(obj)
       self
     end
     
     def assoc(key)
-      self.each do |i|
+      each do |i|
         if i.is_a?(Array) || i.is_a?(NSArray)
           unless i.empty?
             val = i.first
@@ -234,30 +233,36 @@ module OSX
     end
     
     def clear
-      self.removeAllObjects
+      removeAllObjects
       self
     end
     
     def collect!(*args, &block)
-      self.setArray(self.to_a.collect(*args, &block))
+      setArray(collect(*args, &block))
       self
     end
     alias_method :map!, :collect!
     
     # does nothing because NSArray cannot have nil
-    def compact; self.to_a; end
+    def compact; to_a; end
     def compact!; self; end
     
     def concat(other)
-      self.addObjectsFromArray(other)
+      addObjectsFromArray(other)
       self
     end
     
     def delete(val)
-      indexes = []
-      self.each_with_index {|i,n| indexes << n if i.isEqual(val) }
-      indexes.reverse.each {|n| self.removeObjectAtIndex(n) } unless indexes.empty?
-      block_given? ? yield : indexes.empty? ? nil : val
+      set = OSX::NSMutableIndexSet.alloc.init
+      each_with_index {|i,n| set.addIndex(n) if i.isEqual(val) }
+      removeObjectsAtIndexes(set) if set.count > 0
+      if block_given?
+        yield
+      elsif set.count > 0
+        val
+      else
+        nil
+      end
     end
     
     def delete_at(pos)
@@ -267,9 +272,9 @@ module OSX
       count = self.count
       pos = pos.to_i
       pos += count if pos < 0
-      if (0...count).include?(pos)
+      if 0 <= pos && pos < count
         result = self[pos]
-        self.removeObjectAtIndex(pos)
+        removeObjectAtIndex(pos)
         result
       else
         nil
@@ -282,22 +287,22 @@ module OSX
     end
     
     def reject!
-      indexes = []
-      self.each_with_index {|i,n| indexes << n if yield(i) }
-      if indexes.empty?
-        nil
-      else
-        indexes.reverse.each {|n| self.removeObjectAtIndex(n) }
+      set = OSX::NSMutableIndexSet.alloc.init
+      each_with_index {|i,n| set.addIndex(n) if yield(i) }
+      if set.count > 0
+        removeObjectsAtIndexes(set)
         self
+      else
+        nil
       end
     end
     
     def each_index
-      self.each_with_index {|i,n| yield n }
+      each_with_index {|i,n| yield(n) }
     end
     
     def empty?
-      self.count == 0
+      count == 0
     end
     
     def fetch(*args)
@@ -306,31 +311,31 @@ module OSX
       if len == 0 || len > 2
         raise ArgumentError, "wrong number of arguments (#{len} for 2)"
       end
-      nth = args.first
-      unless nth.is_a? Numeric
-        raise TypeError, "can't convert #{nth.class} into Integer"
+      index = args.first
+      unless index.is_a? Numeric
+        raise TypeError, "can't convert #{index.class} into Integer"
       end
-      index = nth.to_i
+      index = index.to_i
       index += count if index < 0
-      if (0...count).include?(index)
-        self.objectAtIndex(index)
+      if 0 <= index && index < count
+        objectAtIndex(index)
       else
         if len == 2
           args[1]
         elsif block_given?
           yield
         else
-          raise IndexError, "index #{nth} out of array"
+          raise IndexError, "index #{args.first} out of array"
         end
       end
     end
 
     def join(*args)
-      self.to_ruby.join(*args)
+      to_ruby.join(*args)
     end
 
     def size
-      self.count
+      count
     end
     alias_method :length, :size
 
@@ -339,16 +344,16 @@ module OSX
       when 0
         ;
       when 1
-        self.addObject(args[0])
+        addObject(args.first)
       else
-        self.addObjectsFromArray(args)
+        addObjectsFromArray(args)
       end
       self
     end
 
     # enable to treat as Array
     def to_ary
-      self.to_a
+      to_a
     end
 
     # comparison between Ruby Array and Cocoa NSArray
@@ -356,7 +361,7 @@ module OSX
       if other.is_a? OSX::NSArray
         isEqualToArray?(other)
       elsif other.respond_to? :to_ary
-        self.to_a == other.to_ary
+        to_a == other.to_ary
       else
         false
       end
@@ -364,7 +369,7 @@ module OSX
 
     def <=>(other)
       if other.respond_to? :to_ary
-        self.to_a <=> other.to_ary
+        to_a <=> other.to_ary
       else
         nil
       end
@@ -381,13 +386,13 @@ module OSX
       ## TODO: should test "respondsToSelector:"
       if Array.public_method_defined?(mname)
         # call as Ruby array
-        rcv = self.to_a
+        rcv = to_a
         org_val = rcv.dup
         result = rcv.send(mname, *args)
         # bang methods modify receiver itself, need to set the new value.
         # if the receiver is immutable, NSInvalidArgumentException raises.
         if rcv != org_val
-          self.setArray(rcv)
+          setArray(rcv)
         end
       else
         # call as objc array
--- src/tests/tc_nsarray.rb
+++ src/tests/tc_nsarray.rb
@@ -4,28 +4,41 @@ require 'osx/cocoa'
 class TC_NSArray < Test::Unit::TestCase
   include OSX
   
+  def alloc_nsarray(*args)
+    ary = NSMutableArray.alloc.init
+    args.each {|i| ary.push(i) }
+    ary
+  end
+  
+  def to_nsarray(ary)
+    NSMutableArray.arrayWithArray(ary)
+  end
+  
+  def map_to_nsnumber(ary)
+    ary.map {|i| NSNumber.numberWithInt(i) }
+  end
+  
+  def map_to_int(ary)
+    ary.map {|i| i.to_i }
+  end
+  
   def test_equal
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
-    ary = [1,2,3].map {|i| NSNumber.numberWithInt(i) }
-    assert(a == ary)
+    a = alloc_nsarray(1,2,3)
+    b = map_to_nsnumber([1,2,3])
+    assert(a == b)
   end
 
   def test_ref
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
-    b = [1, 2, 3, 4, 5]
-    b = b.map {|i| NSNumber.numberWithInt(i) }
+    a = alloc_nsarray(1,2,3,4,5)
+    b = map_to_nsnumber([1,2,3,4,5])
     [-10, -5, -3, 0, 3, 5, 10].each do |i|
       assert_equal(b[i], a[i])
     end
   end
 
   def test_ref_range
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
-    b = [1, 2, 3, 4, 5]
-    b = b.map {|i| NSNumber.numberWithInt(i) }
+    a = alloc_nsarray(1,2,3,4,5)
+    b = map_to_nsnumber([1,2,3,4,5])
     [(0..3), (0...3), (0..5), (0...5), (2..20), (1..-1), (1..-10),
      (-3...-1), (-3..4), (-10...3), (-20...-10), (10..3), (10..20)].each do |i|
       assert_equal(b[i], a[i])
@@ -33,10 +46,8 @@ class TC_NSArray < Test::Unit::TestCase
   end
 
   def test_ref_start_and_length
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
-    b = [1, 2, 3, 4, 5]
-    b = b.map {|i| NSNumber.numberWithInt(i) }
+    a = alloc_nsarray(1,2,3,4,5)
+    b = map_to_nsnumber([1,2,3,4,5])
     [[0,3], [0,10], [0,-3], [-3,2], [-3,10],
      [-3,-3], [-10,2], [3,0], [10,0]].each do |i|
       assert_equal(b[*i], a[*i])
@@ -46,19 +57,17 @@ class TC_NSArray < Test::Unit::TestCase
   def test_assign
     val = 33
     [-5, -3, 0, 3, 5].each do |d|
-      a = NSMutableArray.alloc.init
-      a.push(1, 2, 3, 4, 5)
-      b = [1, 2, 3, 4, 5]
+      a = alloc_nsarray(1,2,3,4,5)
+      b = [1,2,3,4,5]
       a[d] = val
       b[d] = val
-      b = b.map {|i| NSNumber.numberWithInt(i) }
-      assert_equal(NSMutableArray.alloc.initWithArray(b), a)
+      b = map_to_nsnumber(b)
+      assert_equal(to_nsarray(b), a)
     end
   end
   
   def test_assign_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray(1,2,3,4,5)
     assert_raise(IndexError) { a[-10] = 33 }
     assert_raise(IndexError) { a[10] = 33 }
     assert_raise(ArgumentError) { a[3] = nil }
@@ -70,20 +79,18 @@ class TC_NSArray < Test::Unit::TestCase
       [(0..4), (0...4), (0..5), (0...5), (1..15),
        (0...-1), (5..3), (3..10), (1..0), (1..-10),
        (-5...3), (-3..-2)].each do |d|
-        a = NSMutableArray.alloc.init
-        a.push(1, 2, 3, 4, 5)
-        b = [1, 2, 3, 4, 5]
+        a = alloc_nsarray(1,2,3,4,5)
+        b = [1,2,3,4,5]
         a[d] = val
         b[d] = val
-        b = b.map {|i| NSNumber.numberWithInt(i) }
-        assert_equal(NSMutableArray.alloc.initWithArray(b), a)
+        b = map_to_nsnumber(b)
+        assert_equal(to_nsarray(b), a)
       end
     end
   end
   
   def test_assign_range_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray(1,2,3,4,5)
     assert_raise(IndexError) { a[-10..5] = nil }
     assert_raise(IndexError) { a[10..5] = nil }
     assert_nothing_raised { a[5..5] = nil }
@@ -94,20 +101,18 @@ class TC_NSArray < Test::Unit::TestCase
   def test_assign_start_and_length
     [33, nil, [], [11,22,33]].each do |val|
       [[1,3], [1,10], [-3,2], [-3,10]].each do |d|
-        a = NSMutableArray.alloc.init
-        a.push(1, 2, 3, 4, 5)
-        b = [1, 2, 3, 4, 5]
+        a = alloc_nsarray(1,2,3,4,5)
+        b = [1,2,3,4,5]
         a[*d] = val
         b[*d] = val
-        b = b.map {|i| NSNumber.numberWithInt(i) }
-        assert_equal(NSMutableArray.alloc.initWithArray(b), a)
+        b = map_to_nsnumber(b)
+        assert_equal(to_nsarray(b), a)
       end
     end
   end
 
   def test_assign_start_and_length_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray(1,2,3,4,5)
     assert_raise(IndexError) { a[-10,2] = nil }
     assert_raise(IndexError) { a[10,2] = nil }
     assert_raise(IndexError) { a[3,-2] = nil }
@@ -115,162 +120,136 @@ class TC_NSArray < Test::Unit::TestCase
   end
 
   def test_plus
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     a = a + [4,5,6] + a
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     e = [1,2,3]
     e = e + [4,5,6] + e
     assert_equal(e, a)
   end
 
   def test_plus_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_raise(TypeError) { a + 0 }
     assert_nothing_raised { a + NSArray.arrayWithArray([1,2]) }
   end
 
   def test_plus_equal
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     a += [4, 5, 6]
     a += [7, 8, 9]
     b = [1, 2, 3]
     b += [4, 5, 6]
     b += [7, 8, 9]
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     assert_equal(b, a)
   end
 
   def test_multiply
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     a *= 3
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     b = [1,2,3]
     b *= 3
     assert_equal(b, a)
 
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     s = a * ','
     assert_equal([1,2,3]*',', s)
   end
 
   def test_multiply_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_nothing_raised { a * 1.5 }
     assert_raise(TypeError) { a * [] }
   end
 
   def test_minus
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
-    b = NSMutableArray.alloc.init
-    b.push(1, 4)
+    a = alloc_nsarray(1,2,3)
+    b = alloc_nsarray(1,4)
     a = a - b
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     c = [1,2,3]
     c = c - [1,4]
     assert_equal(c, a)
   end
 
   def test_minus_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_nothing_raised { a * 1.5 }
     assert_raise(TypeError) { a * [] }
   end
 
   def test_and
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4)
-    b = NSMutableArray.alloc.init
-    b.push(1, 4, 5)
+    a = alloc_nsarray(1,2,3,4)
+    b = alloc_nsarray(1,4,5)
     a = a & b
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     c = [1,2,3,4]
     c = c & [1,4,5]
     assert_equal(c, a)
     
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4)
-    b = NSMutableArray.alloc.init
-    b.push(6, 7)
+    a = alloc_nsarray(1,2,3,4)
+    b = alloc_nsarray(6,7)
     a = a & b
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     c = [1,2,3,4]
     c = c & [6,7]
     assert_equal(c, a)
   end
   
   def test_and_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_nothing_raised { a & [] }
     assert_raise(TypeError) { a & 5.5 }
   end
 
   def test_or
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4)
-    b = NSMutableArray.alloc.init
-    b.push(1, 4, 5)
+    a = alloc_nsarray(1,2,3,4)
+    b = alloc_nsarray(1,4,5)
     a = a | b
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     c = [1,2,3,4]
     c = c | [1,4,5]
     assert_equal(c, a)
     
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4)
-    b = NSMutableArray.alloc.init
-    b.push(6, 7)
+    a = alloc_nsarray(1,2,3,4)
+    b = alloc_nsarray(6,7)
     a = a | b
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     c = [1,2,3,4]
     c = c | [6,7]
     assert_equal(c, a)
     
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4)
+    a = alloc_nsarray(1,2,3,4)
     a = a | []
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     assert_equal([1,2,3,4]|[], a)
   end
   
   def test_or_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_nothing_raised { a | [] }
     assert_raise(TypeError) { a | 5.5 }
   end
   
   def test_push_op
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     b = NSMutableArray.alloc.init
     b << 1 << 2 << 3
     assert_equal(a, b)
   end
   
   def test_assoc
-    a = NSMutableArray.alloc.init
-    a.push([])
-    a.push(1)
-    a.push([4,5])
-    a.push([8,5])
+    a = alloc_nsarray([], 1, [4,5], [8,5])
     r = a.assoc(4)
-    r = r.map {|i| i.to_i }
+    r = map_to_int(r)
     assert_equal([4,5], r)
   end
   
   def test_at
     [-2, 2, 3].each do |i|
-      a = NSMutableArray.alloc.init
-      a.push(1, 2, 3)
+      a = alloc_nsarray(1,2,3)
       a = a.at(i)
       a = a.to_i if NSNumber === a
       b = [1,2,3]
@@ -280,8 +259,7 @@ class TC_NSArray < Test::Unit::TestCase
   end
   
   def test_at_error
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3)
+    a = alloc_nsarray(1,2,3)
     assert_nothing_raised { a.at(-1) }
     assert_nothing_raised { a.at(30) }
     assert_raise(TypeError) { a.at(nil) }
@@ -289,48 +267,42 @@ class TC_NSArray < Test::Unit::TestCase
   end
 
   def test_clear
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray(1,2,3)
     a.clear
     assert_equal(0, a.size)
   end
 
   def test_collect!
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray(1,2,3,4,5)
     a.collect! {|i| i.to_i * 2 }
-    e = NSMutableArray.alloc.init
-    e.push(2, 4, 6, 8, 10)
+    e = [1,2,3,4,5].map {|i| i*2 }
+    e = map_to_nsnumber(e)
     assert_equal(e, a)
   end
   
   def test_concat
-    a = NSMutableArray.alloc.init
-    a.push(1, 2)
+    a = alloc_nsarray(1,2)
     b = [3,4]
-    b = b.map {|i| NSNumber.numberWithInt(i) }
+    b = map_to_nsnumber(b)
     a.concat(b)
-    e = NSMutableArray.alloc.init
-    e.push(1, 2, 3, 4)
+    e = alloc_nsarray(1,2,3,4)
     assert_equal(e, a)
   end
   
   def test_delete
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 1, 4, 5)
+    a = alloc_nsarray(1,2,1,4,5)
     r = a.delete(1)
     assert_equal(r, 1)
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     e = [1,2,1,4,5]
     r = e.delete(1)
     assert_equal(r, 1)
     assert_equal(e, a)
     
-    a = NSMutableArray.alloc.init
-    a.push(5)
+    a = alloc_nsarray(5)
     r = a.delete(1)
     assert_equal(r, nil)
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     e = [5]
     r = e.delete(1)
     assert_equal(r, nil)
@@ -339,11 +311,10 @@ class TC_NSArray < Test::Unit::TestCase
   
   def test_delete_at
     [-2, 2, 8].each do |d|
-      a = NSMutableArray.alloc.init
-      a.push(1, 2, 1, 4, 5)
+      a = alloc_nsarray(1,2,1,4,5)
       x = a.delete_at(d)
       x = x.to_i if x
-      a = a.map {|i| i.to_i }
+      a = map_to_int(a)
       e = [1,2,1,4,5]
       y = e.delete_at(d)
       assert_equal(x, y)
@@ -352,33 +323,30 @@ class TC_NSArray < Test::Unit::TestCase
   end
   
   def test_delete_if
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 1, 4, 5)
+    a = alloc_nsarray(1,2,1,4,5)
     a.delete_if {|i| i.to_i < 3 }
-    a = a.map {|i| i.to_i }
+    a = map_to_int(a)
     e = [1,2,1,4,5]
     e.delete_if {|i| i < 3 }
     assert_equal(e, a)
   end
   
   def test_each_index
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 1, 4, 5)
+    a = alloc_nsarray(1,2,1,4,5)
     n = 0
     a.each_index {|i| n += 1 }
     assert_equal(a.size, n)
   end
   
   def test_empty?
-    a = NSMutableArray.alloc.init
+    a = alloc_nsarray
     assert_equal(true, a.empty?)
     a.push(1)
     assert_equal(false, a.empty?)
   end
   
   def test_fetch
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 1, 4, 5)
+    a = alloc_nsarray(1,2,1,4,5)
     r = a.fetch(2)
     assert_equal(a[2], r)
     r = a.fetch(-1)
@@ -392,8 +360,7 @@ class TC_NSArray < Test::Unit::TestCase
   end
   
   def test_join
-    a = NSMutableArray.alloc.init
-    a.push(1, [2,3,[4,5]], 6)
+    a = alloc_nsarray(1, [2,3,[4,5]], 6)
     s = a.join(',')
     assert_equal([1,[2,3,[4,5]],6].join(','), s)
 
@@ -407,22 +374,20 @@ class TC_NSArray < Test::Unit::TestCase
   end
   
   def test_join_error
-    a = NSMutableArray.alloc.init
-    a.push(1, [2,3,[4,5]], 6)
+    a = alloc_nsarray(1, [2,3,[4,5]], 6)
     assert_raise(TypeError) { a.join([]) }
     assert_raise(ArgumentError) { a.join(1,2) }
   end
 
   def test_size
-    a = NSMutableArray.alloc.init
-    a.push(1, 2)
+    a = alloc_nsarray(1,2)
     assert_equal(2, a.size)
     assert_equal(2, a.length)
   end
 
   def test_push
-    a = NSMutableArray.alloc.init
-    a.push(1, 2, 3, 4, 5)
+    a = alloc_nsarray
+    a.push(1,2,3,4,5)
     assert_equal(5, a.size)
   end
 end
