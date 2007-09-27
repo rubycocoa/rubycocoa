d056f80c90f5fe9d6d8f7a371efbfc32f104fa3b
psychs <psychs@f4f36d40-9719-0410-9868-dde021a23eda>
2007-09-27 06:32:10 +0000

    2007-09-27  Satoshi Nakagawa <artension@gmail.com>
      * framework/src/objc/objc_compat.h:
      Added #define class_respondsToSelector.

--- src/framework/src/objc/objc_compat.h
+++ src/framework/src/objc/objc_compat.h
@@ -29,6 +29,7 @@
     class_addMethods(klass, mlp);		\
   } \
   while (0)
+#define class_respondsToSelector(klass, sel) ((class_getClassMethod(klass, sel) ?: class_getInstanceMethod(klass, sel)) != NULL)
 
 #define method_getName(method) (((Method)(method))->method_name)
 #define method_getImplementation(method) (((Method)(method))->method_imp)
