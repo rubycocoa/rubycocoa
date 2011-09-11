201ebc925b03033c4e5568d0f3621d9c91fa0160
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2011-09-11 04:16:14 +0000

    add Xcode 4.x file/project templates.

---
+++ src/template/Xcode4.x/Templates/File Templates/Cocoa/Ruby-Cocoa class.xctemplate/NSViewController/___FILEBASENAME___.rb
@@ -0,0 +1,23 @@
+#
+#  ___FILENAME___
+#  ___PROJECTNAME___
+#
+#  Created by ___FULLUSERNAME___ on ___DATE___.
+#  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+class ___FILEBASENAMEASIDENTIFIER___ < OSX::NSObject
+
+  def initWithNibName_bundle_(nibNameOrNil, nibBundleOrNil)
+    self = [super_initWithNibName_bundle_(nibNameOrNil, nibBundleOrNil)
+    if self
+	# Initialization code here.
+    end
+      
+    return self;
+  end
+
+end
+
