7e4c7c9010ea2737970ee87cb4c4842f03c2d9c2
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2011-11-08 15:38:36 +0000

    let to use NSApplicationInit() and NSApplicationMain(), not older-style RBApplicationMain().

--- src/template/Xcode2.x/ProjectBuilder/Application/Cocoa-Ruby Core Data Application/main.m
+++ src/template/Xcode2.x/ProjectBuilder/Application/Cocoa-Ruby Core Data Application/main.m
@@ -11,5 +11,6 @@
 
 int main(int argc, const char *argv[])
 {
-    return RBApplicationMain("rb_main.rb", argc, argv);
+    RBApplicationInit("rb_main.rb", argc, argv, nil);
+    return NSApplicationMain(argc, argv);
 }
--- src/template/Xcode3.x/ProjectBuilder/Application/Cocoa-Ruby Document-based Application/rb_main.rb
+++ src/template/Xcode3.x/ProjectBuilder/Application/Cocoa-Ruby Document-based Application/rb_main.rb
@@ -17,7 +17,4 @@ def rb_main_init
   end
 end
 
-if $0 == __FILE__ then
-  rb_main_init
-  OSX.NSApplicationMain(0, nil)
-end
+rb_main_init
