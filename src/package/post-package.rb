5b1bd2ecd19394b719114ac8bdc8b5404055af88
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2013-10-27 01:03:34 +0000

    Remove packagemaker code, use productbuild to generate .pkg

--- src/package/post-package.rb
+++ src/package/post-package.rb
@@ -20,79 +20,55 @@ Dir.mkdir dmg_dir
   File.chmod(0775, target_dir) if File.exist?(target_dir)
 end
 
-pkgmaker = '/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker'
 pkgbuild = `which pkgbuild`.chomp
 productbuild = `which productbuild`.chomp
 
-if productbuild.length > 0 && File.exist?(productbuild)
-  # generate .pkg with pkgbuild and productbuild
-  dist_dir = File.join(work_dir, 'dist')
-  Dir.mkdir dist_dir
-
-  # collect files into .pkg
-  str = %Q!"#{pkgbuild}" ! +
-	%Q!--root "#{contents_dir}" ! +
-	%Q!--ownership recommended ! +
-	%Q!--identifier "com.fobj.rubycocoa" ! +
-	%Q!--version "#{@config['rubycocoa-version']}" ! +
-	%Q!"#{File.join(dist_dir, package_name)}.pkg" !
-  command str
-  # generate dist.xml
-  str = %Q!"#{productbuild}" ! +
-	%Q!--synthesize ! +
-	%Q!--product "#{File.join(work_dir, 'product.plist')}" ! +
-	%Q!--package "#{File.join(dist_dir, package_name)}.pkg" ! +
-	%Q!"#{File.join(dist_dir, 'dist_base.xml')}" !
-  command str
-  xml = REXML::Document.new(File.read(File.join(dist_dir, 'dist_base.xml')))
-  el_title = REXML::Element.new('title', xml.root)
-  el_title.text = 'RubyCocoa'
-  el_license = REXML::Element.new('license', xml.root)
-  el_license.add_attributes('file' => 'License.txt')
-  el_license = REXML::Element.new('readme', xml.root)
-  el_license.add_attributes('file' => 'ReadMe.html')
-  el_allowed_versions = REXML::Element.new('allowed-os-versions', xml.root)
-  el_os_version = REXML::Element.new('os-version', el_allowed_versions)
-  major, minor = @config['macosx-deployment-target'].split('.', 2)
-  next_os = [major, (minor.to_i + 1).to_s].join('.') # 10.9 => 10.10
-  el_os_version.add_attributes('min' => @config['macosx-deployment-target'], 'before' => next_os)
-  File.open(File.join(dist_dir, 'dist.xml'), 'w') do |f|
-    pf = REXML::Formatters::Default.new
-    pf.write(xml, f)
-  end
-  # complete .pkg
-  str = %Q!"#{productbuild}" ! +
-	%Q!--distribution "#{File.join(dist_dir, 'dist.xml')}" ! +
-	%Q!--resources "#{resources_dir}" ! +
-	%Q!--package-path "#{dist_dir}" ! +
-	%Q!"#{File.join(dmg_dir, package_name)}.pkg" !
-  command str
-else
-  begin
-    command "sudo chown -R root:admin \"#{contents_dir}\""
-    if @config['macosx-deployment-target'].to_f >= 10.7
-      command "sudo chgrp wheel \"#{File.join(contents_dir, '/Library')}\""
-      command "sudo chgrp -R wheel " +
-	      "\"#{File.join(contents_dir, '/Library/Ruby')}\" " +
-	      "\"#{File.join(contents_dir, '/Library/Frameworks')}\""
-    end
-
-    # generate .pkg with PackageMaker.app
-    str = %Q!"#{pkgmaker}" -build ! +
-	  %Q!-p "#{File.join(dmg_dir, package_name)}.pkg" ! +
-	  %Q!-f "#{contents_dir}" -r "#{resources_dir}" ! +
-	  %Q!-i "#{File.join(work_dir, 'Info.plist')}" ! +
-	  %Q!-d "#{File.join(work_dir, 'Description.plist')}" !
-    system str
-    stat = $?.to_i / 256
-    if stat != 0 and stat != 1 # PackageMaker of 10.4 returns 1 (bug)
-      raise RuntimeError, "'system #{str}' failed(#{stat})"
-    end
-  ensure
-    # revert owner
-    command "sudo chown -R `/usr/bin/id -un` \"#{contents_dir}\""
-  end
+unless productbuild.length > 0 && File.exist?(productbuild)
+  raise "command 'productbuild' not found"
 end
+
+# generate .pkg with pkgbuild and productbuild
+dist_dir = File.join(work_dir, 'dist')
+Dir.mkdir dist_dir
+
+# collect files into .pkg
+str = %Q!"#{pkgbuild}" ! +
+      %Q!--root "#{contents_dir}" ! +
+      %Q!--ownership recommended ! +
+      %Q!--identifier "com.fobj.rubycocoa" ! +
+      %Q!--version "#{@config['rubycocoa-version']}" ! +
+      %Q!"#{File.join(dist_dir, package_name)}.pkg" !
+command str
+# generate dist.xml
+str = %Q!"#{productbuild}" ! +
+      %Q!--synthesize ! +
+      %Q!--product "#{File.join(work_dir, 'product.plist')}" ! +
+      %Q!--package "#{File.join(dist_dir, package_name)}.pkg" ! +
+      %Q!"#{File.join(dist_dir, 'dist_base.xml')}" !
+command str
+xml = REXML::Document.new(File.read(File.join(dist_dir, 'dist_base.xml')))
+el_title = REXML::Element.new('title', xml.root)
+el_title.text = 'RubyCocoa'
+el_license = REXML::Element.new('license', xml.root)
+el_license.add_attributes('file' => 'License.txt')
+el_license = REXML::Element.new('readme', xml.root)
+el_license.add_attributes('file' => 'ReadMe.html')
+el_allowed_versions = REXML::Element.new('allowed-os-versions', xml.root)
+el_os_version = REXML::Element.new('os-version', el_allowed_versions)
+major, minor = @config['macosx-deployment-target'].split('.', 2)
+next_os = [major, (minor.to_i + 1).to_s].join('.') # 10.9 => 10.10
+el_os_version.add_attributes('min' => @config['macosx-deployment-target'], 'before' => next_os)
+File.open(File.join(dist_dir, 'dist.xml'), 'w') do |f|
+  pf = REXML::Formatters::Default.new
+  pf.write(xml, f)
+end
+# complete .pkg
+str = %Q!"#{productbuild}" ! +
+      %Q!--distribution "#{File.join(dist_dir, 'dist.xml')}" ! +
+      %Q!--resources "#{resources_dir}" ! +
+      %Q!--package-path "#{dist_dir}" ! +
+      %Q!"#{File.join(dmg_dir, package_name)}.pkg" !
+command str
 
 %w(License.txt LGPL
     ../../tmpl/ReadMe.html ../../tmpl/ReadMe.ja.html).each do |f|
