ce4a8de0e48a987eee6ca8f259b924867558ec90
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2013-11-03 19:36:20 +0000

    Generate Objective-C documents under doc/objc/ with headerdoc

--- src/framework/pre-doc.rb
+++ src/framework/pre-doc.rb
@@ -1,12 +1,15 @@
 require 'fileutils'
 
-unless File.exist?('bridge-doc')
-  begin
-    Dir.chdir('tool') do
-      ruby("gen_bridge_doc.rb build ../bridge-doc")
-    end
-  rescue RuntimeError => e
-    rm_rf 'bridge-doc'
-    raise e
-  end
+# Objective-C documents by "headerdoc"
+hd2html = `xcrun -f headerdoc2html`.chomp
+gatherhd = `xcrun -f gatherheaderdoc`.chomp
+if hd2html.length > 0 && File.exist?(hd2html)
+    FileUtils.rm_r('../doc/objc/', :force => true)
+    cmd = %W(#{hd2html} -o ../doc/objc)
+    cmd += %w(src/objc/RubyCocoa.h src/objc/RBRuntime.h src/objc/RBObject.h)
+    command(cmd.join(' '))
+    cmd = %W(#{gatherhd} ../doc/objc RubyCocoa.html)
+    command(cmd.join(' '))
 end
+
+# TODO: Ruby documents by "yard"
