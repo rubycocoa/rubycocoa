b26740e888d5c11c00baf9a797f45843b4503095
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2013-11-06 13:52:58 +0000

    Generate Ruby documents under doc/ruby/ with yardoc

--- src/framework/pre-doc.rb
+++ src/framework/pre-doc.rb
@@ -1,3 +1,4 @@
+# vim:sw=4:ts=8
 require 'fileutils'
 
 # Objective-C documents by "headerdoc"
@@ -13,3 +14,24 @@ if hd2html.length > 0 && File.exist?(hd2html)
 end
 
 # TODO: Ruby documents by "yard"
+yardoc = `which yardoc`.chomp
+if yardoc.length > 0 && File.exist?(yardoc)
+    FileUtils.rm_r('../doc/ruby/', :force => true)
+    cmd = %W(#{yardoc} -o ../doc/ruby --markup markdown
+             --load ./tool/yard_objc_register.rb
+	     --title "RubyCocoa\ Documentation"
+    )
+    # defined in Ruby
+#    cmd += Dir.glob("src/ruby/**/*.rb")
+#    cmd -= %w(src/ruby/osx/objc/cocoa.rb
+#	      src/ruby/osx/objc/oc_all.rb
+#	      src/ruby/osx/objc/foundation.rb)
+    # defined in Objective-C
+    cmd += %w(src/objc/cls_objcid.m
+	      src/objc/cls_objcptr.m
+	      src/objc/mdl_bundle_support.m
+	      src/objc/mdl_objwrapper.m
+	      src/objc/mdl_osxobjc.m
+	      src/objc/BridgeSupport.m)
+    command(cmd.join(' '))
+end
