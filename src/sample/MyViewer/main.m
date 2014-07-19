e6b19b1a80af210cf1a893df63df94bdb1b7daa7
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2014-07-19 07:38:57 +0000

    Fix MyViewer.app do not launch

--- src/sample/MyViewer/main.m
+++ src/sample/MyViewer/main.m
@@ -6,11 +6,12 @@
  *
  **/
 
+#import <AppKit/NSApplication.h>
 #import <RubyCocoa/RBRuntime.h>
 
 int
 main(int argc, const char* argv[])
 {
-    RBApplicationInit("rb_main.rb", argc, (const char **)argv, nil);
+    RBApplicationInit("main.rb", argc, (const char **)argv, nil);
     return NSApplicationMain(argc, (const char **)argv);
 }
