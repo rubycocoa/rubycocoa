0afb866d1a659f9c86c75bbb0ddc4cb27186ebcd
kimuraw <kimuraw@f4f36d40-9719-0410-9868-dde021a23eda>
2014-07-27 12:49:02 +0000

    Add ruby code coverage with simplecov
    coverage enables via environment COVERAGE.
    
        $ COVERAGE=YES rake test
            or
        $ COVERAGE=YES ruby install.rb test

--- src/tests/testall.rb
+++ src/tests/testall.rb
@@ -6,8 +6,20 @@ rescue LoadError
 end
 require 'test/unit'
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.configure do
+    root '../'
+    add_group 'Framework', 'framework'
+    add_group 'Tests', 'tests'
+  end
+  SimpleCov.start
+end
+
 `ls tc_*.rb`.each_line do |testcase|
   testcase.chop!
   $stderr.puts testcase if $VERBOSE
   require(File.join('.', testcase ))
 end
+
+Test::Unit::AutoRunner.run
